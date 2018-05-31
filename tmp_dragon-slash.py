28928af010eb72de41675a171bde7f7d87cccd63
Gustavo Rehermann <gugurehermann@gmail.com>
2018-05-31 02:09:19 -0300

    whoops

--- tmp_dragon-slash.py
+++
@@ -1,6 +0,0 @@
-
-def slash(entity):
-    if entity.attr['target'] is None:
-        return
-
-    entity.call('attack', entity.world.from_id(entity['target']), __import__('random').uniform(30, 50))
